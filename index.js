658299caa26087835d0501d1f22677435fa054dc
shayan <sak704@nyu.edu>
2022-01-30 15:36:08 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Code your solutions in this file
+/*
+for (let age = 30; age<40; age++){
+    console.log(`I'm ${age} years old. Happy Birthday to me!`);
+   // debugger;
+   
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+
+function writeCards(name,event) {    
+    
+    let msgArray = [];
+    let initString = `Thank you,`;
+    let giftString = `for the wonderful ${event} gift!`;
+    
+    for (let i=0; i<name.length ; i++){     
+        
+        debugger;
+
+        msgArray [i] = `${initString} ${name[i]}, ${giftString}`;
+
+        //return msgArray[i];
+
+        
+
+    }
+
+    return msgArray;       
+    
+}
+
+function countDown(i) {
+
+    let count = i;
+
+    while (count > -1){
+        console.log(count);
+        count--;
+    }
+    
+}
+
+
+
+
